9232add4c9a956424cbe93f1897824b4ad77aa35
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-03-28 13:41:04 -0700

    task 11

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,6 +8,7 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
+from api.v1.views.places_reviews import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
