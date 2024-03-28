53fb0500fca7b24f417246ef4f0b98dd1448805d
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-03-28 08:14:17 -0700

    task 2 and 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""index file"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ return the status of API """
+    return jsonify({"status": "OK"})
