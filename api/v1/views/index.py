11bc9ad0b68eb49c0b089412b72fdf67e0c3884e
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-03-28 09:00:58 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,27 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ return the status of API """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_counter():
+    """retrieve the number of each object"""
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    number = {}
+    for i in range(len(classes)):
+        number[names[i]] = storage.count(classes[i])
+    return jsonify(number)
