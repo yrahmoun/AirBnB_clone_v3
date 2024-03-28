0e1a7ae6c5308c76de7e57e377837af18fdb69fd
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-03-28 09:06:33 -0700

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """flask application"""
 
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def close_stoage(error):
     """close Storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 error handler"""
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
